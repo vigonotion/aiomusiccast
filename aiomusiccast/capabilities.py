26fee22f27d713e7f408e4ce68120a12d358656d
Michael Harbarth <michael.harbarth@gmx.de>
2021-11-18 17:34:26 +0100

    Implemented requested changes. Use RangeStep in NumberSetter Capability.

--- aiomusiccast/capabilities.py
+++ aiomusiccast/capabilities.py
@@ -2,8 +2,10 @@ from abc import ABC
 from enum import Enum
 from typing import Any, Callable, Dict
 
+from aiomusiccast.musiccast_data import RangeStep
 
-class EntityTypes(Enum):
+
+class EntityType(Enum):
     """Type of the Capability."""
     REGULAR = 1  # For major features
     CONFIG = 2  # For features to configure the device or a zone
@@ -15,45 +17,45 @@ class Capability(ABC):
     """Base class for all capabilities."""
     id: str
     name: str
-    entity_type: EntityTypes
-    get_current: Callable
+    entity_type: EntityType
+    get_value: Callable
 
-    def __init__(self, capability_id, name, entity_type, get_current):
+    def __init__(self, capability_id, name, entity_type, get_value):
         """
         Initialize the base class and set general vars.
         @param capability_id: Unique ID of this capability
         @param name: Name that should be displayed in a UI
         @param entity_type: Define of what type this capability is
-        @param get_current: Callable to get the current values of this capability
+        @param get_value: Callable to get the current values of this capability
         """
         self.id = capability_id
         self.name = name
         self.entity_type = entity_type
-        self.get_current = get_current
+        self.get_value = get_value
 
     @property
     def current(self) -> Any:
-        return self.get_current()
+        return self.get_value()
 
 
-class SetableCapability(Capability, ABC):
+class SettableCapability(Capability, ABC):
     """Base class for a capability, which is not read only."""
-    set_current: Callable
+    set_value: Callable
 
-    def __init__(self, capability_id, name, entity_type, get_current, set_current):
+    def __init__(self, capability_id, name, entity_type, get_value, set_value):
         """
         Initialize the setable base class.
         @param capability_id: Unique ID of this capability
         @param name: Name that should be displayed in a UI
         @param entity_type: Define of what type this capability is
-        @param get_current: Callable to get the current values of this capability
-        @param set_current: Callable to set the value. Should only expect the new values as parameter
+        @param get_value: Callable to get the current values of this capability
+        @param set_value: Callable to set the value. Should only expect the new values as parameter
         """
-        super().__init__(capability_id, name, entity_type, get_current)
-        self.set_current = set_current
+        super().__init__(capability_id, name, entity_type, get_value)
+        self.set_value = set_value
 
     async def set(self, value):
-        await self.set_current(value)
+        await self.set_value(value)
 
 
 class NumberSensor(Capability):
@@ -68,45 +70,48 @@ class TextSensor(Capability):
     pass
 
 
-class NumberSetter(SetableCapability):
+class NumberSetter(SettableCapability):
     """Class to set numbers"""
-    min_value: float
-    max_value: float
-    step: float
+    value_range: RangeStep
 
-    def __init__(self, capability_id, name, entity_type, get_current, set_current, min_value, max_value, step):
+    def __init__(self, capability_id, name, entity_type, get_value, set_value, min_value, max_value, step):
         """
         Initialize a NumberSetter
         @param capability_id: Unique ID of this capability
         @param name: Name that should be displayed in a UI
         @param entity_type: Define of what type this capability is
-        @param get_current: Callable to get the current values of this capability
-        @param set_current: Callable to set the value. Should only expect the new values as parameter
+        @param get_value: Callable to get the current values of this capability
+        @param set_value: Callable to set the value. Should only expect the new values as parameter
         @param min_value: Minimum value, which can be set
         @param max_value: Maximum value, which can be set
         @param step: The step between minimum and maximum
         """
-        super().__init__(capability_id, name, entity_type, get_current, set_current)
-        self.min_value = min_value
-        self.max_value = max_value
-        self.step = step
+        super().__init__(capability_id, name, entity_type, get_value, set_value)
+        self.value_range = RangeStep()
+        self.value_range.minimum = min_value
+        self.value_range.maximum = max_value
+        self.value_range.step = step
+        
+    async def set(self, value):
+        self.value_range.check(value)
+        await super(NumberSetter, self).set(value)
 
 
-class OptionSetter(SetableCapability):
+class OptionSetter(SettableCapability):
     """A capability to set a value from a list of valid options."""
     options: Dict[str, str]
 
-    def __init__(self, capability_id, name, entity_type, get_current, set_current, options):
+    def __init__(self, capability_id, name, entity_type, get_value, set_value, options):
         """
         Initialize a option setter
         @param capability_id: Unique ID of this capability
         @param name: Name that should be displayed in a UI
         @param entity_type: Define of what type this capability is
-        @param get_current: Callable to get the current values of this capability
-        @param set_current: Callable to set the value. Should only expect the new values as parameter
+        @param get_value: Callable to get the current values of this capability
+        @param set_value: Callable to set the value. Should only expect the new values as parameter
         @param options: A dictionary of valid options with the option as key and a label as value
         """
-        super().__init__(capability_id, name, entity_type, get_current, set_current)
+        super().__init__(capability_id, name, entity_type, get_value, set_value)
         self.options = options
 
     async def set(self, value):
@@ -115,6 +120,9 @@ class OptionSetter(SetableCapability):
         await super(OptionSetter, self).set(value)
 
 
-class BinarySetter(SetableCapability):
+class BinarySetter(SettableCapability):
     """A class to set boolean values."""
-    pass
+    async def set(self, value):
+        if not isinstance(value, bool):
+            raise ValueError("The given value is not a boolean value")
+        await super().set(value)
--- aiomusiccast/capability_registry.py
+++ aiomusiccast/capability_registry.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from .features import ZoneFeature, DeviceFeature
-from .capabilities import OptionSetter, EntityTypes, NumberSetter, BinarySetter, Capability
+from .capabilities import OptionSetter, EntityType, NumberSetter, BinarySetter, Capability
 
 """Dictionary of all DeviceFeatures with a callable as value. 
 The callable expects an ID and a MusicCastDevice as parameters."""
@@ -7,7 +9,7 @@ _device_capabilities = {
     DeviceFeature.DIMMER: lambda capability_id, device: NumberSetter(
         capability_id,
         "Display Brightness",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.dimmer.dimmer_current,
         lambda value: device.set_dimmer(int(value)),
         device.data.dimmer.minimum,
@@ -17,21 +19,21 @@ _device_capabilities = {
     DeviceFeature.SPEAKER_A: lambda capability_id, device: BinarySetter(
         capability_id,
         "Speaker A",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.speaker_a,
         lambda val: device.set_speaker_a(val),
     ),
     DeviceFeature.SPEAKER_B: lambda capability_id, device: BinarySetter(
         capability_id,
         "Speaker B",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.speaker_b,
         lambda val: device.set_speaker_b(val),
     ),
     DeviceFeature.PARTY_MODE: lambda capability_id, device: BinarySetter(
         capability_id,
         "Party Mode",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.party_enable,
         lambda val: device.set_party_mode(val),
     ),
@@ -44,7 +46,7 @@ _zone_capabilities = {
     ZoneFeature.SURR_DECODER_TYPE: lambda capability_id, device, zone_id: OptionSetter(
         capability_id,
         "Surround Decoder Device",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].surr_decoder_type,
         lambda val: device.set_surround_decoder(zone_id, val),
         {key: key for key in device.data.zones[zone_id].surr_decoder_type_list},
@@ -52,7 +54,7 @@ _zone_capabilities = {
     ZoneFeature.SLEEP: lambda capability_id, device, zone_id: OptionSetter(
         capability_id,
         "Sleep Timer",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].sleep_time,
         lambda val: device.set_sleep_timer(zone_id, val),
         {0: "off", 30: "30 min", 60: "60 min", 90: "90 min", 120: "120 min"},
@@ -61,15 +63,15 @@ _zone_capabilities = {
         "mode": lambda capability_id, device, zone_id: OptionSetter(
             capability_id,
             "Equalizer Mode",
-            EntityTypes.CONFIG,
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].equalizer_mode,
             lambda val: device.set_equalizer(zone_id, mode=val),
             {key: key for key in device.data.zones[zone_id].equalizer_mode_list},
         ),
         "low": lambda capability_id, device, zone_id: NumberSetter(
             capability_id,
-            "Low",
-            EntityTypes.CONFIG,
+            "Equalizer Low",
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].equalizer_low,
             lambda val: device.set_equalizer(zone_id, low=int(val)),
             device.data.zones[zone_id].range_step["equalizer"].minimum,
@@ -78,8 +80,8 @@ _zone_capabilities = {
         ),
         "mid": lambda capability_id, device, zone_id: NumberSetter(
             capability_id,
-            "Mid",
-            EntityTypes.CONFIG,
+            "Equalizer Mid",
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].equalizer_mid,
             lambda val: device.set_equalizer(zone_id, mid=int(val)),
             device.data.zones[zone_id].range_step["equalizer"].minimum,
@@ -88,8 +90,8 @@ _zone_capabilities = {
         ),
         "high": lambda capability_id, device, zone_id: NumberSetter(
             capability_id,
-            "High",
-            EntityTypes.CONFIG,
+            "Equalizer High",
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].equalizer_high,
             lambda val: device.set_equalizer(zone_id, high=int(val)),
             device.data.zones[zone_id].range_step["equalizer"].minimum,
@@ -100,16 +102,16 @@ _zone_capabilities = {
     ZoneFeature.TONE_CONTROL: {
         "mode": lambda capability_id, device, zone_id: OptionSetter(
             capability_id,
-            "Tone Mode",
-            EntityTypes.CONFIG,
+            "Tone Control Mode",
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].tone_mode,
             lambda val: device.set_tone_control(zone_id, mode=val),
             {key: key for key in device.data.zones[zone_id].tone_control_mode_list},
         ),
         "bass": lambda capability_id, device, zone_id: NumberSetter(
             capability_id,
-            "Bass",
-            EntityTypes.CONFIG,
+            "Tone Control Bass",
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].tone_bass,
             lambda val: device.set_tone_control(zone_id, bass=int(val)),
             device.data.zones[zone_id].range_step["tone_control"].minimum,
@@ -118,8 +120,8 @@ _zone_capabilities = {
         ),
         "treble": lambda capability_id, device, zone_id: NumberSetter(
             capability_id,
-            "Treble",
-            EntityTypes.CONFIG,
+            "Tone Control Treble",
+            EntityType.CONFIG,
             lambda: device.data.zones[zone_id].tone_treble,
             lambda val: device.set_tone_control(zone_id, treble=int(val)),
             device.data.zones[zone_id].range_step["tone_control"].minimum,
@@ -130,7 +132,7 @@ _zone_capabilities = {
     ZoneFeature.DIALOGUE_LEVEL: lambda capability_id, device, zone_id: NumberSetter(
         capability_id,
         "Dialogue Level",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].dialogue_level,
         lambda val: device.set_dialogue_level(zone_id, int(val)),
         device.data.zones[zone_id].range_step["dialogue_level"].minimum,
@@ -140,7 +142,7 @@ _zone_capabilities = {
     ZoneFeature.DIALOGUE_LIFT: lambda capability_id, device, zone_id: NumberSetter(
         capability_id,
         "Dialogue Lift",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].dialogue_lift,
         lambda val: device.set_dialogue_lift(zone_id, int(val)),
         device.data.zones[zone_id].range_step["dialogue_lift"].minimum,
@@ -150,7 +152,7 @@ _zone_capabilities = {
     ZoneFeature.DTS_DIALOGUE_CONTROL: lambda capability_id, device, zone_id: NumberSetter(
         capability_id,
         "DTS Dialogue Control",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].dts_dialogue_control,
         lambda val: device.set_dts_dialogue_control(zone_id, int(val)),
         device.data.zones[zone_id].range_step["dts_dialogue_control"].minimum,
@@ -160,7 +162,7 @@ _zone_capabilities = {
     ZoneFeature.LINK_AUDIO_DELAY: lambda capability_id, device, zone_id: OptionSetter(
         capability_id,
         "Link Audio Delay",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].link_audio_delay,
         lambda val: device.set_link_audio_delay(zone_id, val),
         {key: key for key in device.data.zones[zone_id].link_audio_delay_list},
@@ -168,7 +170,7 @@ _zone_capabilities = {
     ZoneFeature.LINK_CONTROL: lambda capability_id, device, zone_id: OptionSetter(
         capability_id,
         "Link Control",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].link_control,
         lambda val: device.set_link_control(zone_id, val),
         {key: key for key in device.data.zones[zone_id].link_control_list},
@@ -176,7 +178,7 @@ _zone_capabilities = {
     ZoneFeature.LINK_AUDIO_QUALITY: lambda capability_id, device, zone_id: OptionSetter(
         capability_id,
         "Link Audio Quality",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].link_audio_quality,
         lambda val: device.set_link_audio_quality(zone_id, val),
         {key: key for key in device.data.zones[zone_id].link_audio_quality_list},
@@ -184,42 +186,42 @@ _zone_capabilities = {
     ZoneFeature.BASS_EXTENSION: lambda capability_id, device, zone_id: BinarySetter(
         capability_id,
         "Bass Extension",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].bass_extension,
         lambda val: device.set_bass_extension(zone_id, val),
     ),
     ZoneFeature.EXTRA_BASS: lambda capability_id, device, zone_id: BinarySetter(
         capability_id,
         "Extra Bass",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].extra_bass,
         lambda val: device.set_extra_bass(zone_id, val),
     ),
     ZoneFeature.ENHANCER: lambda capability_id, device, zone_id: BinarySetter(
         capability_id,
         "Enhancer",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].enhancer,
         lambda val: device.set_enhancer(zone_id, val),
     ),
     ZoneFeature.PURE_DIRECT: lambda capability_id, device, zone_id: BinarySetter(
         capability_id,
         "Pure Direct",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].pure_direct,
         lambda val: device.set_pure_direct(zone_id, val),
     ),
     ZoneFeature.ADAPTIVE_DRC: lambda capability_id, device, zone_id: BinarySetter(
         capability_id,
         "Adaptive DRC",
-        EntityTypes.CONFIG,
+        EntityType.CONFIG,
         lambda: device.data.zones[zone_id].adaptive_drc,
         lambda val: device.set_adaptive_drc(zone_id, val),
     ),
 }
 
 
-def build_device_capabilities(device: "MusicCastDevice") -> list[Capability]:
+def build_device_capabilities(device: "MusicCastDevice") -> List[Capability]:
     """
     Function to build all Capabilities of a given device.
     The ID of the capabilities will be set to '{feature.name.lower()}_{key}'
@@ -239,7 +241,7 @@ def build_device_capabilities(device: "MusicCastDevice") -> list[Capability]:
     return result
 
 
-def build_zone_capabilities(device: "MusicCastDevice", zone_id) -> list[Capability]:
+def build_zone_capabilities(device: "MusicCastDevice", zone_id) -> List[Capability]:
     """
     Function to build all Capabilities of a given zone of a device.
     The ID of the capabilities will be set to 'zone_{feature.name.lower()}_{key}'
